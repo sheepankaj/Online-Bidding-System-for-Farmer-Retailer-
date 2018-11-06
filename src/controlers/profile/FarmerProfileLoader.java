7cb3388b80b5592b262111ec76cc587036f01b93
Sree <gsreelekshmi@yahoo.com>
2018-11-06 15:47:41 +0000

    commit

--- src/controlers/profile/FarmerProfileLoader.java
+++ src/controlers/profile/FarmerProfileLoader.java
@@ -8,6 +8,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.entity.User;
+import models.profile.ProfilesService;
+
 /**
  * Servlet implementation class FarmerProfileLoader
  */
@@ -35,8 +38,8 @@ public class FarmerProfileLoader extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String username = request.getParameter("username");
+		User user = ProfilesService.getProfileServiceInstance(getServletContext()).getProfile(username);
 	}
 
 }
