78f2ae2cb026799b8e43cc13d3c28354acfc6ebd
sultanH786 <sulthan.mdg@gmail.com>
2018-11-12 16:42:16 +0530

    changes for payment service

--- src/models/payment/PaymentService.java
+++ src/models/payment/PaymentService.java
@@ -1,6 +1,13 @@
 package models.payment;
 
-public class PaymentService {
-	////
+import models.entity.Contract;
 
+public class PaymentService {
+
+	void makePayment(Contract contract)
+	{
+		long retailerID = contract.getRetailerUserID();
+		double finalCost = contract.getPriceOnFrequency();
+		
+	}
 }
