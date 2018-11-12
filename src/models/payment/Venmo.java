780bf26467b183efd5b794731acfa2c641cc0692
Sree <gsreelekshmi@yahoo.com>
2018-11-12 11:07:38 +0000

    commit

---
+++ src/models/payment/Venmo.java
@@ -0,0 +1,35 @@
+package models.payment;
+
+public class Venmo implements IBankAccount {
+
+	@Override
+	public boolean validateAccount() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void makePayment(double amount) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public StringBuilder printReceipt() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double checkBalance() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void addCash() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
