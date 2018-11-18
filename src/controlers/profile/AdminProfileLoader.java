5b71d4bcce512e7ff261089f7092413ca63ef2a8
Pankaj Kumar Shee <sheepankaj@gmail.com>
2018-11-18 10:20:37 +0000

    Logout function

--- src/controlers/profile/AdminProfileLoader.java
+++ src/controlers/profile/AdminProfileLoader.java
@@ -54,9 +54,33 @@ public class AdminProfileLoader extends HttpServlet {
 		User user = ProfilesService.getProfileServiceInstance(getServletContext()).getProfile(username);
 		response.getWriter().append("{\"state\":\"Success\",\"message\":\"Login Successfull..!!\",\"page\":\""+user.getUsername()+"\",\"id\":"+user.getUserID()+"}");
 		
-		
 			
 		
+	}
+	
+	
+	@SuppressWarnings("serial")
+	@WebServlet("/logout")
+	public class LogoutServlet extends HttpServlet {
+
+	    @Override
+	    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    	HttpSession session = request.getSession();
+	    	session.removeAttribute("username");
+	    	session.invalidate();
+	        //request.getSession().invalidate();
+	        //response.sendRedirect(request.getContextPath() + "../index.jsp");
+	    	
+	    response.sendRedirect("../index.jsp");
+	    
+	     response.setHeader("Cache-Control"," no-cache, no-store, must-revalidate");
+	    
+	    if(session.getAttribute("username")==null)
+	  	  response.sendRedirect("../index.jsp");
+	  	  
+	    
+	    }
+
 	}
 
 }
